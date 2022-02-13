9c7ffd8145733ea9b594a063483dab8e9545918b
akkayy <ahmed.kaja@outlook.com>
2022-02-13 15:10:06 +1100

    Displaying 'Feed (global)' & 'PopularTags' (placeholders) in home page

---
+++ src/components/feed/Feed.vue
@@ -0,0 +1,3 @@
+<script setup></script>
+
+<template>Global Feeds</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,37 @@
 <template>
-	<main>Home page</main>
+	<div class="home-page">
+		<div class="banner">
+			<div class="container">
+				<h1 class="logo-font">conduit</h1>
+				<p>A place to share your knowledge.</p>
+			</div>
+		</div>
+		<div class="container page">
+			<div class="row">
+				<div class="col-md-9">
+					<Suspense>
+						<template #default>
+							<Feed />
+						</template>
+						<template #fallback>
+							Articles are downloading...
+						</template>
+					</Suspense>
+				</div>
+
+				<div class="col-md-3">
+					<div class="sidebar">
+						<Suspense>
+							<template #default>
+								<PopularTags />
+							</template>
+							<template #fallback>
+								Popular tags are downloading...
+							</template>
+						</Suspense>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
